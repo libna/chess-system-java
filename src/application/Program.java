6abce7c848218c6bcd316386515dbae8c2c9b6a7
libna <libnarodrigues723@gmail.com>
2021-01-26 22:43:59 -0300

    Primeira Classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Position position = new Position(1,1);
+		System.out.println(position);
 	}
 }
